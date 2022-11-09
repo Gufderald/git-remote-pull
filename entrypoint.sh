4dd8cac706515a7d8cd5331dd2d42e87e12f0ad0
Gufderald <den17112000@gmail.com>
2022-11-09 15:24:17 +0300

    moved from deprecated checkout

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 git remote add target https://${INPUT_TARGET_USERNAME}:${INPUT_TARGET_TOKEN}@${INPUT_TARGET_URL#https://}
+git fetch target
+git fetch origin
 
 git config --global user.name "github-actions[bot]"
 git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"
 
 {
-  git checkout target/${GITHUB_REF_NAME}
+  git switch target/${GITHUB_REF_NAME}
 } || {
   echo $?
   exit 1
